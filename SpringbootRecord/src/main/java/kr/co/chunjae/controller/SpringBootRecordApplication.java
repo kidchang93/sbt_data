0a1676ac4de9c8b7d886f0f7f81efb518b6876e1
kidchang93 <lck930721@gmail.com>
2024-01-20 17:02:12 +0900

    SpringBoot 게시판 개인실습-controller

--- SpringbootRecord/src/main/java/kr/co/chunjae/controller/SpringBootRecordApplication.java
+++ SpringbootRecord/src/main/java/kr/co/chunjae/controller/SpringBootRecordApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sbt01Application {
+public class SpringBootRecordApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sbt01Application.class, args);
+		SpringApplication.run(SpringBootRecordApplication.class, args);
 	}
 
 }
