37f01917e432f93a0da6ccaf37ff31f3b29f19d9
kidchang93 <lck930721@gmail.com>
2024-01-22 09:44:36 +0900

    JUnit 테스트 ( NoTransactionServiceTest )

--- transaction/src/test/java/com/practice/transaction/noTransaction/NoTransactionServiceTest.java
+++ transaction/src/test/java/com/practice/transaction/noTransaction/NoTransactionServiceTest.java
@@ -1,5 +1,8 @@
 package com.practice.transaction.noTransaction;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,5 +13,14 @@ class NoTransactionServiceTest {
   private Long SENDER_ID = 1L;
   private Long RECEIVER_ID = 2L;
   private Long AMOUNT = 5000L;
+
+  @Autowired
+  private NoTransactionService service;
+
+  @Test
+  @DisplayName("이체 테스트 001 : 정상 이체")
+  void testTransfer_001(){
+    service.transfer(SENDER_ID,RECEIVER_ID,AMOUNT);
+  }
 
 }
