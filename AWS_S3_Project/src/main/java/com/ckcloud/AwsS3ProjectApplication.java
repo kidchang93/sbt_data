14e16213c8ed647210b34a333f2dbe8c68222fbc
kidchang93 <lck930721@gmail.com>
2024-01-23 16:42:16 +0900

    aws S3 적용 프로젝트

---
+++ AWS_S3_Project/src/main/java/com/ckcloud/AwsS3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ckcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3ProjectApplication.class, args);
+    }
+
+}
---
+++ AWS_S3_Project/src/main/java/com/ckcloud/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ckcloud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AwsS3ProjectApplication.class);
+    }
+
+}
