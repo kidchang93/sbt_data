41ebef357b7aca1884f1e3c46940d6c2fd49faf4
kidchang93 <lck930721@gmail.com>
2024-04-11 17:04:16 +0900

    JPA repo 상속

--- TestProject/src/main/java/com/example/testprogect/data/repository/ProductRepository.java
+++ TestProject/src/main/java/com/example/testprogect/data/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.testprogect.data.repository;
 
-public interface ProductRepository {
+import com.example.testprogect.data.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
 }
