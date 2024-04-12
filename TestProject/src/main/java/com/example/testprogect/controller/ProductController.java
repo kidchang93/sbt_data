821cfc886b6ca0881d74ee836d6e3e98edc443d6
kidchang93 <lck930721@gmail.com>
2024-04-12 15:03:44 +0900

    Logback 출력 확인 , 문법 적용

--- TestProject/src/main/java/com/example/testprogect/controller/ProductController.java
+++ TestProject/src/main/java/com/example/testprogect/controller/ProductController.java
@@ -2,12 +2,15 @@ package com.example.testprogect.controller;
 
 import com.example.testprogect.data.dto.ProductDTO;
 import com.example.testprogect.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     @Autowired
@@ -17,7 +20,19 @@ public class ProductController {
 
     @GetMapping(value = "/product/{productId}")
     public ProductDTO getProduct(@PathVariable String productId){
-        return productService.getProduct(productId);
+
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[getProduct] perform {} of Around Hub API.", "getProduct");
+
+        ProductDTO productDTO = productService.getProduct(productId);
+        LOGGER.info("[getProduct] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms",
+                productDTO.getProductId(),
+                productDTO.getProductName(),
+                productDTO.getProductPrice(),
+                productDTO.getProductStock(),
+                (System.currentTimeMillis() - startTime));
+
+        return productDTO;
 
     }
 
