3c9705aeedf904ad59d9a97984982ba50fa8db73
kidchang93 <lck930721@gmail.com>
2024-04-19 15:10:02 +0900

    ShortUrl (단축URL) 마지막 작업

--- TestProject/src/main/java/com/example/testprogect/data/dao/ShortUrlDAO.java
+++ TestProject/src/main/java/com/example/testprogect/data/dao/ShortUrlDAO.java
@@ -6,4 +6,9 @@ public interface ShortUrlDAO {
     ShortUrlEntity saveShortUrl(ShortUrlEntity shortUrlEntity);
 
     ShortUrlEntity getShortUrl(String originalUrl);
+
+    ShortUrlEntity getOriginalUrl(String shortUrl);
+    ShortUrlEntity updateShortUrl(ShortUrlEntity newShortUrl);
+    void deleteByShortUrl(String shortUrl);
+    void deleteByOriginalUrl(String originalUrl);
 }
--- TestProject/src/main/java/com/example/testprogect/data/dao/impl/ShortUrlDAOImpl.java
+++ TestProject/src/main/java/com/example/testprogect/data/dao/impl/ShortUrlDAOImpl.java
@@ -3,6 +3,7 @@ package com.example.testprogect.data.dao.impl;
 import com.example.testprogect.data.dao.ShortUrlDAO;
 import com.example.testprogect.data.entity.ShortUrlEntity;
 import com.example.testprogect.data.repository.ShortUrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,7 @@ public class ShortUrlDAOImpl implements ShortUrlDAO {
 
     private final ShortUrlRepository shortUrlRepository;
 
+    @Autowired
     public ShortUrlDAOImpl(ShortUrlRepository shortUrlRepository) {
         this.shortUrlRepository = shortUrlRepository;
     }
@@ -24,5 +26,34 @@ public class ShortUrlDAOImpl implements ShortUrlDAO {
     public ShortUrlEntity getShortUrl(String originalUrl) {
         ShortUrlEntity foundShortUrl = shortUrlRepository.findByOrgUrl(originalUrl);
         return foundShortUrl;
+    }
+
+    @Override
+    public ShortUrlEntity getOriginalUrl(String shortUrl) {
+        ShortUrlEntity foundShortUrl = shortUrlRepository.findByUrl(shortUrl);
+        return foundShortUrl;
+    }
+
+    @Override
+    public ShortUrlEntity updateShortUrl(ShortUrlEntity newShortUrl) {
+
+        ShortUrlEntity foundShortUrl = shortUrlRepository.findByOrgUrl(newShortUrl.getOrgUrl());
+        foundShortUrl.setUrl(newShortUrl.getUrl());
+        ShortUrlEntity savedShortUrl = shortUrlRepository.save(foundShortUrl);
+
+        return savedShortUrl;
+    }
+
+
+    @Override
+    public void deleteByShortUrl(String shortUrl) {
+        ShortUrlEntity foundShortUrl = shortUrlRepository.findByUrl(shortUrl);
+        shortUrlRepository.delete(foundShortUrl);
+    }
+
+    @Override
+    public void deleteByOriginalUrl(String originalUrl) {
+        ShortUrlEntity foundShortUrl = shortUrlRepository.findByOrgUrl(originalUrl);
+        shortUrlRepository.delete(foundShortUrl);
     }
 }
